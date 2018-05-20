20d404270152d7f8f194f78ff01218bb40857873
Kolchin <skolchin@kpmg.com>
2018-05-20 17:05:06 +0300

    Added few more tests

--- tests/testthat/test_eqvispkg.r
+++ tests/testthat/test_eqvispkg.r
@@ -10,9 +10,22 @@ test_that('Test file loading and cleaning', {
   expect_error(df %>% mutate(LONGITUDE = "Test" %>% eq_clean_data() ) )
 })
 
-test_that('Test plot layer', {
+test_that('Test plot visualizations', {
   t <- geom_timeline(data = df, aes(x=DATE))
   expect_that(t, is_a("ggproto") )
   t <- geom_timeline_label(data = df, aes(x=DATE, label = LOCATION_NAME))
   expect_that(t, is_a("ggproto") )
+  t <- theme_timeline()
+  expect_that(t, is_a("theme") )
 })
+
+test_that('Test leaflet visualizations', {
+  fn <- system.file('extdata', "signif.txt", package = 'eqvispkg')
+  df <- eq_load_data(fn)
+  df <- df[df$I_D == 5595,]
+  l <- eq_create_label( data = df )
+  expect_equal (nchar(l), 105 )
+  m <- eq_map(data = df, annot_col = "DATE")
+  expect_that(m, is_a("leaflet") )
+})
+
